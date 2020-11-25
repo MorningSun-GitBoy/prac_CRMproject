89973b1cc5a098777fcae5ac4de30510704d6edd
Bachelor <AndyWolfCF2013@163.com>
2020-11-25 19:55:51 +0800

    delete useless code

--- prac_CRMsystem/src/main/java/com/neau/crm/web/dao/ActivityRemarkDelDao.java
+++ prac_CRMsystem/src/main/java/com/neau/crm/web/dao/ActivityRemarkDelDao.java
@@ -4,7 +4,6 @@ import com.neau.crm.web.domain.ActivityRemark;
 import com.neau.crm.web.domain.fordelet.DelContainer;
 
 import java.util.List;
-import java.util.Map;
 
 public interface ActivityRemarkDelDao {
     public int insertItems(List<DelContainer<ActivityRemark>> acRemDelList);
